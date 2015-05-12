a44f1ae29b0d25a407ceca560f0c44876f38c1d9
liqrgv <septianhari@gmail.com>
2015-05-12 09:56:31 +0700

    initial commit

---
+++ application/controllers/Auth.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Prodase
+ * Date: 12/05/2015
+ * Time: 8:50
+ */
+
+class Auth extends CI_Controller {
+
+    public function login()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+
+}
